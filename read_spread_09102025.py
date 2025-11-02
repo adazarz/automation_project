e6f18fd2f92ebb158d3a90435e568b7b4496e061
adazarz <zarzyckiadam@tutanota.com>
2025-11-02 14:55:20 +0100

    Update read_spread_09102025.py

--- read_spread_09102025.py
+++ read_spread_09102025.py
@@ -116,7 +116,7 @@ def connecting(numbers):
         y += 1
 
 #Reading the data from excel macro
-excel = pd.read_excel(r"C:\Users\azarzyck\Desktop\BOE_Posting_MA20_new_procedure_Beta.xlsm")
+excel = pd.read_excel(PATH_TO_EXCEL_FILE)
 clients = excel["Unnamed: 1"][3:].to_list()
 nlcn = excel["Unnamed: 3"][3:].to_list()
 names = excel["Unnamed: 2"][3:].to_list()
@@ -320,8 +320,8 @@ for key in final_data:
     session.findById("wnd[0]").sendVKey(0)
     # Filling in the customer number and company code
     session.findById("wnd[0]/usr/ctxtDD_KUNNR-LOW").Text = key
-    session.findById("wnd[0]/usr/ctxtDD_BUKRS-LOW").Text = "MA20"
-    session.findById("wnd[0]/usr/ctxtPA_VARI").Text = "WEKSLE ADAM"
+    session.findById("wnd[0]/usr/ctxtDD_BUKRS-LOW").Text = COMPANY_CODE
+    session.findById("wnd[0]/usr/ctxtPA_VARI").Text = LAYOUT_NAME
     # Ensuring that normal items are checked
     session.findById("wnd[0]/usr/chkX_NORM").selected = True
     # Executing
@@ -541,4 +541,5 @@ for key in final_data:
 ex = input("Write anything to exit the program\n").lower()
 
 while ex not in list("{:c}".format(x) for x in range(97, 123)) + list(str(x) for x in range(10)):
+
     ex = input("Write anything to exit the program\n").lower()
